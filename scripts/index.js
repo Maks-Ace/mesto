c5154d3912856ae4f8df89b4b886ed28d0581f8d
Maksim Tuzov <makstuzoff@yandex.ru>
2020-07-12 15:19:44 +0300

    feat: Реальизовать минимальные требования проекта

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,45 @@
-// Var declaration
+// Объявление переменных
+let popup = document.querySelector(".popup");
 let editButoon = document.querySelector(".profile__edit-button");
 let nameValue = document.querySelector('.profile__name');
+let activityValue = document.querySelector('.profile__activity');
+let nameInput = document.querySelector('.popup__input_name');
+let activityInput = document.querySelector('.popup__input_activity');
+let formElement = document.querySelector('.popup__container');
+let closeButton = document.querySelector('.popup__close-button');
 
-editButoon.addEventListener('click', function(){
-  alert(nameValue.innerHTML);
+// Объявление функций
+
+function pageLoad() {
+  nameInput.value = nameValue.textContent;
+  activityInput.value = activityValue.textContent;
+}
+
+function closeForm() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Отмена стандартной отправки формы
+
+  // Обновить значения на страницу
+  nameValue.textContent = nameInput.value;
+  activityValue.textContent = activityInput.value;
+  // Закрыть форму
+  closeForm();
+}
+
+
+// Обработка событий
+
+editButoon.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
 });
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+closeButton.addEventListener('click', closeForm);
+
+
+// Вызов функции при каждой загрузке страницы
+pageLoad();
